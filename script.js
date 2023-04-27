3c3b1d133e126afc83a050a5d7d854b5fc7f18fe
costin2121 <dobrecostindragos@gmail.com>
2023-04-27 10:35:51 +0300

    added a slight change for mobile

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello, World!");
+const clickpress = document.getElementById("clickpress");
+let onMobile = !!navigator.userAgent.match(/iphone|android|blackberry/ig) || false;
+
+if (onMobile) {
+    clickpress.innerText = "Press";
+} else {
+    clickpress.innerText = "Click";
+}
